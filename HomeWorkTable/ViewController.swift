85d3067752519ce8888bd584dce389552be0ce4c
mrs4z <mrs4z@icloud.com>
2021-10-14 09:34:40 +0300

    Initial Commit

---
+++ HomeWorkTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkTable
+//
+//  Created by Александр Горденко on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
