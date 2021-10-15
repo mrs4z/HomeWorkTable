4e59f075fdd00bfe8aad2f3b8a84b14b10ad719f
mrs4z <mrs4z@icloud.com>
2021-10-15 12:43:45 +0300

    final append categories with items to table

--- HomeWorkTable/TableController.swift
+++ HomeWorkTable/TableController.swift
@@ -72,13 +72,70 @@ final class TableController: UIViewController, UITableViewDelegate, UITableViewD
             }),
             .description(item: TableItemDescription(title: "Wi-Fi", description: "Не подключено", icon: UIImage(systemName: "wifi"), iconBg: .systemBlue) {
                 print("CLICK")
+            }),
+            .description(item: TableItemDescription(title: "Bluetooth", description: "Вкл.", icon: UIImage(systemName: "iphone.homebutton.radiowaves.bluetooth"), iconBg: .systemBlue) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Сотовая связь", icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), iconBg: .systemGreen) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Режим модема", icon: UIImage(systemName: "personalhotspot"), iconBg: .systemGreen) {
+                print("CLICK")
             })
         ]))
         
         items.append(TableCategorie(items: [
             .simple(item: TableItemSimple(title: "Уведомления", icon: UIImage(systemName: "bell"), iconBg: .systemRed) {
                 print("CLICK")
-            })
+            }),
+            .simple(item: TableItemSimple(title: "Звуки, тактильные сигналы", icon: UIImage(systemName: "speaker.wave.3"), iconBg: .systemRed) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Фокусирование", icon: UIImage(systemName: "moon.fill"), iconBg: .systemIndigo) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Экранное время", icon: UIImage(systemName: "hourglass"), iconBg: .systemIndigo) {
+                print("CLICK")
+            }),
+        ]))
+        
+        items.append(TableCategorie(items: [
+            .simple(item: TableItemSimple(title: "Основные", icon: UIImage(systemName: "gear"), iconBg: .systemGray) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Пункт управления", icon: UIImage(systemName: "switch.2"), iconBg: .systemGray) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Экран и яркость", icon: UIImage(systemName: "textformat.size"), iconBg: .systemBlue) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Экран «Домой»", icon: UIImage(systemName: "house.fill"), iconBg: .systemIndigo) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Универсальный доступ", icon: UIImage(systemName: "figure.wave.circle"), iconBg: .systemBlue) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Обои", icon: UIImage(systemName: "newspaper.fill"), iconBg: .systemCyan) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Siri и Поиск", icon: UIImage(systemName: "magnifyingglass"), iconBg: .systemIndigo) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "FaceID и код-пароль", icon: UIImage(systemName: "faceid"), iconBg: .systemGreen) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Экстренный вызов - SOS", icon: UIImage(systemName: "questionmark.circle.fill"), iconBg: .systemRed) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Уведомления о контакте", icon: UIImage(systemName: "staroflife.fill"), iconBg: .systemBrown) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Аккумулятор", icon: UIImage(systemName: "battery.100"), iconBg: .systemGreen) {
+                print("CLICK")
+            }),
+            .simple(item: TableItemSimple(title: "Конфиденциальность", icon: UIImage(systemName: "hand.raised.fill"), iconBg: .systemBlue) {
+                print("CLICK")
+            }),
         ]))
     }
     
